abe9db4c4ca50e8a1e1b092ceec09e718211bfa8
Faraaz2497 <76690389+Faraaz2497@users.noreply.github.com>
2022-01-29 17:47:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -14,3 +14,21 @@ function take_snapshot() {
     })
 }
 console.log("ml5version"+ml5.version);
+
+var classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/YmJxP__1s/model.json",modelLoaded)
+function modelLoaded(){
+    console.log("Model Is Loaded")
+}
+function check(){
+    img=document.getElementById("captured_image");
+    classifier.classify(img,gotResult);
+}
+function gotResult(error,results){
+    if(error){
+        console.log(results);
+    }
+    else{
+        document.getElementById("result_objectname").innerHTML=results[0].label
+        document.getElementById("result_objectaccuracy").innerHTML=result[0].confidence
+    }
+}
