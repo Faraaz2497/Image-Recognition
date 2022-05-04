c72da4ccba4ddc83311392d2d1573432005ff119
Faraaz2497 <76690389+Faraaz2497@users.noreply.github.com>
2022-05-04 17:45:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,49 +1,26 @@
-      
-Webcam.attach( '#camera' );
-
-camera = document.getElementById("camera");
-      
-  Webcam.set({
-    width:350,
-    height:300,
-    image_format : 'png',
-    png_quality:90
-  });
-
-function take_snapshot()
-{
-    Webcam.snap(function(data_uri) {
-        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
-    });
+function preload(){
 }
-
-  console.log('ml5 version:', ml5.version);
-  
-  // Initialize the Image Classifier method with MobileNet
-//classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JACkTMciY/model.json',modelLoaded);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json',modelLoaded);
-
-  // When the model is loaded
-  function modelLoaded() {
-    console.log('Model Loaded!');
-  }
-      
-  function check()
-  {
-    img = document.getElementById('selfie_image');
-    classifier.classify(img, gotResult);
-  }
-
-
-// A function to run when we get any errors and the results
-function gotResult(error, results) {
-  // Display error in the console
-  if (error) {
-    console.error(error);
-  } else {
-    // The results are in an array ordered by confidence.
-    console.log(results);
-    document.getElementById("result_object_name").innerHTML = results[0].label;
-    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
-  }
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',getPoses)
+}
+function draw(){
+    image(video,0,0,300,300);
+}
+function takeSnapshot(){
+    save('Mustache/Lipstick Image.png');
+}
+function getPoses(result){
+    if (result.length>0){
+        console.log(result);
+        console.log("nose x="+result[0].pose.nose.x);
+        console.log("nose y="+result[0].pose.nose.y);
+    }
+}
+function modelLoaded(){
+    console.log("Model is Loaded");
 }
